fe3fc73765eafee684fe12fe3cbe0e180b112acb
pranavkirtani <pranavkirtani@gmail.com>
2017-03-21 02:06:25 +0530

    some changes

--- server.py
+++ server.py
@@ -6,11 +6,14 @@ pd.options.mode.chained_assignment = None
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
 # list of stopwords like articles, preposition
-#stop = set(stopwords.words('english'))
+stop = set(stopwords.words('english'))
 from string import punctuation
 from collections import Counter
 import re
 import numpy as np
 import os
 trainFile=os.path.basename("news.csv")
-data = pd.read_csv(trainFile)
+data = pd.read_csv(trainFile, encoding = "ISO-8859-1")
+data.head()
+print('data shape:', data.shape)
+data.category.value_counts().plot(kind='bar', grid=True, figsize=(16, 9))
